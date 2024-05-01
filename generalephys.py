f7317f4e3f5938a5c1672cfd8aafd4cb5a54839f
daniel denman <danieljdenman@gmail.com>
2024-05-01 09:23:49 -0600

    add cleanAxes to utils, remove from standalone and generalephys

--- generalephys.py
+++ generalephys.py
@@ -2114,28 +2114,6 @@ def plotExchangeFromPSTH_2(data,unit,setcolor,UV='#4B0082',Green='#6B8E23',datao
                 #axis.set_title(expt_names[i]+' unit: '+unit)
             
 
-            
-def cleanAxes(ax,bottomLabels=False,leftLabels=False,rightLabels=False,topLabels=False,total=False):
-    ax.tick_params(axis='both',labelsize=10)
-    ax.spines['top'].set_visible(False);
-    ax.yaxis.set_ticks_position('left');
-    ax.spines['right'].set_visible(False);
-    ax.xaxis.set_ticks_position('bottom')
-    if not bottomLabels or topLabels:
-        ax.set_xticklabels([])
-    if not leftLabels or rightLabels:
-        ax.set_yticklabels([])
-    if rightLabels:
-        ax.spines['right'].set_visible(True);
-        ax.spines['left'].set_visible(False);
-        ax.yaxis.set_ticks_position('right');
-    if total:
-        ax.set_frame_on(False);
-        ax.set_xticklabels('',visible=False);
-        ax.set_xticks([]);
-        ax.set_yticklabels('',visible=False);
-        ax.set_yticks([])
-
 def summarize_color(data,unit,color_folder=''):
     fig = plt.figure(figsize=(8.5,11))
 
--- utils.py
+++ utils.py
@@ -398,3 +398,24 @@ def get_spike_limits(nwb_data):
     lasts = [np.array(nwb_data['processing'][nwb_data['processing'].keys()[0]]['UnitTimes'][other]['times'])[-1]\
               for other in np.array(nwb_data['processing'][nwb_data['processing'].keys()[0]]['UnitTimes']['unit_list'])]
     return np.min(firsts),np.max(lasts)
+
+def cleanAxes(ax,bottomLabels=False,leftLabels=False,rightLabels=False,topLabels=False,total=False):
+    ax.tick_params(axis='both',labelsize=10)
+    ax.spines['top'].set_visible(False);
+    ax.yaxis.set_ticks_position('left');
+    ax.spines['right'].set_visible(False);
+    ax.xaxis.set_ticks_position('bottom')
+    if not bottomLabels or topLabels:
+        ax.set_xticklabels([])
+    if not leftLabels or rightLabels:
+        ax.set_yticklabels([])
+    if rightLabels:
+        ax.spines['right'].set_visible(True);
+        ax.spines['left'].set_visible(False);
+        ax.yaxis.set_ticks_position('right');
+    if total:
+        ax.set_frame_on(False);
+        ax.set_xticklabels('',visible=False);
+        ax.set_xticks([]);
+        ax.set_yticklabels('',visible=False);
+        ax.set_yticks([])
